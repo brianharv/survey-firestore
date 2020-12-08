0a255d03d06492befac44e5beddcfa76ccab21d6
Brian Harvey <brian.harv3y@gmail.com>
2020-12-08 09:11:27 -0800

    add navbar
    CO-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
     <React.Fragment>
-      <h1>This is the header</h1>
-      <h2>A Navbar will go here</h2>
+      <Navbar>
+        <Navbar.Brand href="#home">Navbar navbar navbar</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">username</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
     </React.Fragment>
   )
 }
