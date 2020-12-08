9e77ea543e06cff75c272c4d48d954c10395d106
Brian Harvey <brian.harv3y@gmail.com>
2020-12-08 09:29:41 -0800

    add NewSurveyForm and basic conditional rendering
    CO-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- src/components/SurveyControl.js
+++ src/components/SurveyControl.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import SurveyList from './SurveyList';
+import NewSurveyForm from './NewSurveyForm';
 
 
 class SurveyControl extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      formVisibleOnPage: false
+    }
   }  
 
-  render () {
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage}));
+    }
 
+
+  render () {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewSurveyForm />
+      buttonText = "Return to Survey List";
+    } else {
+      currentlyVisibleState = <SurveyList />
+      buttonText = "Create New Survey";
+    }
     return (
       <React.Fragment>
-        <h1>SurveyControl</h1>
-          <SurveyList/>
+       {currentlyVisibleState}
+       <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
